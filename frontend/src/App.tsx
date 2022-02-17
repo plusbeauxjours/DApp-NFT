6819461f8f6df7f1ebfd9cf5e0693f3b75c2675f
plusbeauxjours <plusbeauxjours@gmail.com>
2022-02-17 21:20:21 +0700

    set: added web3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,36 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./routes/Main";
 
 interface IProps {}
 
 const App: React.FC<IProps> = () => {
+  const [account, setAccount] = useState<string>("");
+
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        setAccount(accounts[0]);
+      } else {
+        alert("Install Metamask");
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getAccount();
+  }, []);
+
+  useEffect(() => {
+    console.log("account", account);
+  }, [account]);
+
   return (
     <BrowserRouter>
       <Routes>
